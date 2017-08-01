3d883f229abd6408c0d4d5fed001ba0530595124
David Corson-Knowles <david.corsonknowles@gmail.com>
2017-08-01 10:10:28 -0700

    Install webpack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+let webpack = require("webpack");
+
+let plugins = []; // if using any plugins for both dev and production
+let devPlugins = []; // if using any plugins for development
+
+let prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
+
+module.exports = {
+  context: __dirname,
+  entry: './hangman.js',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    rules: [
+            {
+                test:   /\.css$/,
+                use: ["style-loader", "css-loader", "postcss-loader"]
+            }
+        ],
+    loaders: [
+      {
+        test: /.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
