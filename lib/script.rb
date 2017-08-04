69a584a536a945428832fae85898e4faec9b2295
David Corson-Knowles <david.corsonknowles@gmail.com>
2017-08-04 11:58:09 -0700

    Minor updates

--- lib/script.rb
+++ lib/script.rb
@@ -1,6 +1,6 @@
 array = File.readlines('test_output.txt').map { |e| "#{e.chomp}," }
 
-file_name = 'test_output3.txt'
+file_name = 'test_output1.txt'
 File.open(file_name, "a+") do |f|
   f.puts array
 end
